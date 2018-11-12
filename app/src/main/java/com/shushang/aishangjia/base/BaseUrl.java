800f5249de42923714ae416df006a76c76dc7c0b
ntrer <929727871@qq.com>
2018-11-12 11:02:40 +0800

    submit

--- app/src/main/java/com/shushang/aishangjia/base/BaseUrl.java
+++ app/src/main/java/com/shushang/aishangjia/base/BaseUrl.java
@@ -2,10 +2,10 @@ package com.shushang.aishangjia.base;
 
 public class BaseUrl {
 
-        public final static String BASE_URL="http://ejjz.shushangsoft.com/";
+//        public final static String BASE_URL="http://ejjz.shushangsoft.com/";
 
 
-//    public final static String BASE_URL="http://192.168.0.55:8999/";
+    public final static String BASE_URL="http://192.168.0.55:8999/";
 
 //    public final static String BASE_URL="http://192.168.0.55/";
 
